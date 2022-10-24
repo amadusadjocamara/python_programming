fb32990969f8fa2ca00c39cab48356d232113d33
Amadu Sadjo Camara <105577816+amadusadjocamara@users.noreply.github.com>
2022-10-25 04:50:15 +0530

    python

---
+++ class_program/cond/pro01.py
@@ -0,0 +1,10 @@
+from pydoc import stripid
+
+
+name = str(input("Enter your name: ")).strip()
+if name == "Amadu":
+    print("beodfull name {}".format(name))
+else:
+    print("simple name {}".format(name))
+print("good morning.")
+
