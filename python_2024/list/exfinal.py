d8449a834d262b801d5238680c166438b3c89639
Amadu Sadjo Camara <105577816+amadusadjocamara@users.noreply.github.com>
2024-04-07 16:10:52 +0530

    python
    python

--- python_2024/list/exfinal.py
+++ python_2024/list/exfinal.py
@@ -2,5 +2,6 @@ shopping_list = ["Milk", "Eggs", "Bread", "Bananas"]
 print(shopping_list)
 shopping_list.append("Apples")
 shopping_list [3] = "Mussa"
+shopping_list.remove("Bread")
 print(shopping_list.pop(3))
 print(shopping_list)
